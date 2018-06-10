5a8e3a600df218914628edfe859c68b7381cb2ec
Matt Monsour <mzmonsour@gmail.com>
2018-06-10 10:08:15 -0400

    Implement iterator over model triangles

--- src/model.cpp
+++ src/model.cpp
@@ -1,4 +1,6 @@
 #include "model.h"
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 
 Model::Model(  const std::string name,
                 std::vector<glm::vec4> vertices,
@@ -9,4 +11,95 @@ Model::Model(  const std::string name,
     m_normals(std::move(normals)),
     m_triangles(std::move(triangles))
 {
+}
+
+Model::triangle_iter Model::begin() const
+{
+    return Model::triangle_iter(*this, m_triangles.cbegin());
+}
+
+Model::triangle_iter Model::end() const
+{
+    return Model::triangle_iter(*this, m_triangles.cend());
+}
+
+Model::triangle_iter::triangle_iter(const Model& m, std::vector<FaceIndex>::const_iterator iter) :
+    m_container(m),
+    m_idx_iter(iter),
+    m_current(m)
+{
+}
+
+Model::triangle_iter& Model::triangle_iter::operator++()
+{
+    //  pre-inc
+    ++m_idx_iter;
+    return *this;
+}
+
+Model::triangle_iter Model::triangle_iter::operator++(int)
+{
+    // post-inc
+    triangle_iter old(*this);
+    m_idx_iter++;
+    return old;
+}
+
+bool Model::triangle_iter::operator==(Model::triangle_iter& b) const
+{
+    return (&m_container == &b.m_container) && (m_idx_iter == b.m_idx_iter);
+}
+
+bool Model::triangle_iter::operator!=(Model::triangle_iter& b) const
+{
+    return !(*this == b);
+}
+
+Model::triangle& Model::triangle_iter::operator*()
+{
+    m_current.m_idx = &(*m_idx_iter);
+    return m_current;
+}
+
+Model::triangle * Model::triangle_iter::operator->()
+{
+    m_current.m_idx = &(*m_idx_iter);
+    return &m_current;
+}
+
+Model::triangle::triangle(const Model& m) :
+    m_container(m),
+    m_idx(nullptr)
+{
+}
+
+glm::vec4 Model::triangle::p0() const
+{
+    return m_container.get_vertices()[m_idx->vertices[0]];
+}
+
+glm::vec4 Model::triangle::p1() const
+{
+    return m_container.get_vertices()[m_idx->vertices[1]];
+}
+
+glm::vec4 Model::triangle::p2() const
+{
+    return m_container.get_vertices()[m_idx->vertices[2]];
+}
+
+glm::vec4 Model::triangle::surface_normal(glm::vec3 coords) const
+{
+    glm::vec4 n0, n1, n2;
+    auto& normals = m_container.get_normals();
+    n0 = normals[m_idx->normals[0]];
+    n1 = normals[m_idx->normals[1]];
+    n2 = normals[m_idx->normals[2]];
+    return coords.x * n0 + coords.y * n1 + coords.z * n2;
+}
+
+glm::vec2 Model::triangle::surface_uvs(glm::vec3 coords) const
+{
+    // STUB
+    return glm::vec2(0.0, 0.0);
 }
--- src/model.h
+++ src/model.h
@@ -20,6 +20,74 @@ class Model {
 
     public:
 
+        class triangle_iter;
+
+        class triangle {
+            private:
+
+                const Model& m_container;
+                const FaceIndex* m_idx;
+
+                friend class Model::triangle_iter;
+
+            public:
+
+                triangle(const Model& m);
+                ~triangle() {}
+
+                /**
+                 * Get the first vertex of the triangle.
+                 */
+                glm::vec4 p0() const;
+
+                /**
+                 * Get the second vertex of the triangle.
+                 */
+                glm::vec4 p1() const;
+
+                /**
+                 * Get the third vertex of the triangle.
+                 */
+                glm::vec4 p2() const;
+                
+                /**
+                 * Compute the normal at a point on the surface of the triangle.
+                 *
+                 * @param coords Barycentric coordinates of the point on the surface.
+                 */
+                glm::vec4 surface_normal(glm::vec3 coords) const;
+
+                /**
+                 * Compute the UV coordinates at a point on the surface of the triangle.
+                 *
+                 * @param coords Barycentric coordinates of the point on the surface.
+                 */
+                glm::vec2 surface_uvs(glm::vec3 coords) const;
+        };
+
+        class triangle_iter {
+            private:
+
+                const Model& m_container;
+                std::vector<FaceIndex>::const_iterator m_idx_iter;
+                triangle m_current;
+
+            public:
+
+                triangle_iter(const Model& m, std::vector<FaceIndex>::const_iterator iter);
+                ~triangle_iter() {}
+
+                triangle_iter& operator++();
+                triangle_iter operator++(int);
+
+                bool operator==(triangle_iter& b) const;
+                bool operator!=(triangle_iter& b) const;
+
+                triangle& operator*();
+                triangle * operator->();
+
+        };
+
         /**
          * Create a model from a list of vertices, normals, and triangles.
          */
@@ -41,4 +109,19 @@ class Model {
          * Get the model's vertices.
          */
         const std::vector<glm::vec4>& get_vertices() const { return m_vertices; }
+
+        /**
+         * Get the model's normals.
+         */
+        const std::vector<glm::vec4>& get_normals() const { return m_normals; }
+
+        /**
+         * Beginning iterator over all triangles in the model.
+         */
+        triangle_iter begin() const;
+
+        /**
+         * Ending iterator over all triangles in the model.
+         */
+        triangle_iter end() const;
 };
