e4f9cc0797fca2580c6f6ec09355bff5843f9a1e
Matt Monsour <mzmonsour@gmail.com>
2018-06-18 20:26:36 -0400

    Accuracy improvements to ray tracer
    Using pre-computed normal saves computations, and appears to give
    greater accuracy at camera facing edges. Adding a margin of error makes
    rays less likely to pass through mesh seams.

--- src/trace.cpp
+++ src/trace.cpp
@@ -3,6 +3,8 @@
 #include <glm/geometric.hpp>
 #include <limits>
 
+const static scalar ERROR_THOLD = 0.00005;
+
 trace_info trace_ray(const ray& r, const std::vector<Model>& objs)
 {
     trace_info info;
@@ -17,7 +19,8 @@ trace_info trace_ray(const ray& r, const std::vector<Model>& objs)
             p2 = tri.p2();
             // Compute plane intersection
             // TODO Should norm be calculated, or provided?
-            vec3 norm = glm::cross(vec3(p1-p0), vec3(p2-p0));
+            // Pre-calculated norm appears to be more accurate.
+            vec3 norm = tri.surface_normal(vec3(1.0, 0.0, 0.0));
             plane = vec4(norm, -glm::dot(norm, vec3(p0)));
             d = -glm::dot(plane, r.origin) / glm::dot(plane, r.dir);
             if (d < 0 || d >= best_d) {
@@ -25,6 +28,7 @@ trace_info trace_ray(const ray& r, const std::vector<Model>& objs)
                 continue;
             }
             pout = r.origin + d * r.dir;
+
             // Compute barycenter
             vec4 r, q1, q2;
             scalar q1q1, q1q2, q2q2;
@@ -43,7 +47,7 @@ trace_info trace_ray(const ray& r, const std::vector<Model>& objs)
             qmat[1][1] = q1q1;
             qmat =  (((scalar)1.0) / (q1q1 * q2q2 - q1q2 * q1q2)) * qmat;
             w = qmat * rq;
-            if (w.x >= 0.0 && w.y >= 0.0 && w.x + w.y <= 1.0) {
+            if (w.x >= 0.0 - ERROR_THOLD && w.y >= 0.0 - ERROR_THOLD && w.x + w.y <= 1.0 + ERROR_THOLD) {
                 // Barycenter is valid; Point lies within triangle
                 info.hitobj = &obj;
                 info.hitpos = pout;
