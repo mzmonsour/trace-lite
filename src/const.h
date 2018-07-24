d2d2ccc2bfa29f1a8701a77ebf6e9c8c5ea42bfa
Matt Monsour <mzmonsour@gmail.com>
2018-07-24 11:58:31 -0400

    Make constant for identity matrix for code clarity

--- src/const.h
+++ src/const.h
@@ -1,8 +1,11 @@
 #include "types.h"
 #include <limits>
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
 const static scalar SCALAR_INF = std::numeric_limits<scalar>::infinity();
 
 const static vec3 VEC3_MAXIMUM = vec3(SCALAR_INF, SCALAR_INF, SCALAR_INF);
 const static vec3 VEC3_MINIMUM = -VEC3_MAXIMUM;
+
+const static mat4 MAT4_IDENTITY = mat4((scalar)1.0);
