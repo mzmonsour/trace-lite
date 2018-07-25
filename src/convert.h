5311559a85368eb41e453e36c9918518dfa4ebf3
Matt Monsour <mzmonsour@gmail.com>
2018-07-24 21:32:30 -0400

    Fix assimp matrix order in convert function

--- src/convert.h
+++ src/convert.h
@@ -12,10 +12,10 @@
  */
 inline mat4 assimp_mat_to_glm(const aiMatrix4x4& mat)
 {
-    return glm::mat4(   mat.a1, mat.a2, mat.a3, mat.a4,
-                        mat.b1, mat.b2, mat.b3, mat.b4,
-                        mat.c1, mat.c2, mat.c3, mat.c4,
-                        mat.d1, mat.d2, mat.d3, mat.d4);
+    return glm::mat4(   mat.a1, mat.b1, mat.c1, mat.d1,
+                        mat.a2, mat.b2, mat.c2, mat.d2,
+                        mat.a3, mat.b3, mat.c3, mat.d3,
+                        mat.a4, mat.b4, mat.c4, mat.d4);
 }
 
 inline vec4 assimp_vec_to_glm4(const aiVector3D& vec, scalar w)
